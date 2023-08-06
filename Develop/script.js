1c9cabeb0f725a77fb6ff14c63b7b5bcdc66b22c
Sean Keller <sean.keller.sk@gmail.com>
2023-08-05 19:26:59 -0700

    added var for allChacters, password, and charCount

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-//Variable Arrays//
+//Variable Arrays
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -21,3 +21,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//creates a function that returns what shows up in the text field of the password text box.
+function generatePassword () {
+}
+
+//array of all the characters combined
+var allCharacters = [];
+
+
+//array that will return the password
+var password = []; 
+
+//this will appear as a prompt when the generate password button is pressed
+
+var charCount = prompt("Choose a password with length of 8-128 characters.");
+
+if (isNaN(charCount)) {
+
+return "The value you have entered is invalid";
+
+}
+
+
+
+
+
+
+
